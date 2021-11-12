096f85cf521826ed9b09fe94e2e2f9c58bc960ce
Anuarul Irfan <75465459+Anuarul-Irfan@users.noreply.github.com>
2021-11-12 23:38:09 +0800

    Create BlockStatement2.java

---
+++ BlockStatement2.java
@@ -0,0 +1,24 @@
+package selfprogramming;
+
+import java.util.Scanner;
+
+public class BlockStatement2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int amount = 10;
+		
+		System.out.print("Enter balance: ");
+		int balance = sc.nextInt();
+		
+		if (amount <= balance) {
+			balance = balance - amount;   
+			System.out.println("Acct new balance = " + balance);
+		}
+
+
+	}
+
+}
